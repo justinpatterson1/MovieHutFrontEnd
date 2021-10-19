7b671471b0b96300a042c41e483d95a82c97d526
Justin Patterson <justin.patterson17@yahoo.com>
2021-10-18 23:21:17 -0400

    sign up form connected to db

--- src/components/Administration Pages/SignUp.js
+++ src/components/Administration Pages/SignUp.js
@@ -1,50 +1,87 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+
 
 const SignUp = () => {
+
+    const [user,setUser] = useState({
+        name:"",
+        address:"",
+        email:"",
+        password:""
+    })
+    const addNewUser = (evt)=>{
+        evt.preventDefault();
+
+        fetch('http://localhost:4000/users',{
+            method:'POST',
+            headers:{'Content-Type': 'application/json'},
+            body:JSON.stringify(user)
+
+            
+        })
+        .catch(err=>{console.log(`Error:${err}`)})
+        
+        console.log(user)
+    }
     return (
         <div className='grid col-1' style={{alignItems:'center' , height:'100vh'}}>
             <div className='' style={{width:'30%',margin:'0 auto'}}>
             <h1 style={{textAlign:'center',color:"white", fontSize:'2rem'}}>Sign Up</h1>
+                <form onSubmit={(evt)=>{addNewUser(evt)}}>
                 <div>
                         <div class="field">
                             <label class="label has-text-white">Name</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="e.g Alex Smith"/>
+                                <input class="input" type="text" value={user.name} placeholder="e.g Alex Smith" onChange={(evt)=>{
+                                    setUser({
+                                        ...user,
+                                         name:evt.target.value
+                                    })
+                                }}/>
                             </div>
                         </div>
 
                         <div class="field">
                             <label class="label has-text-white">Address</label>
                             <div class="control">
-                                <input class="input" type="email" placeholder="e.g 120 Sesame Street"/>
+                                <input class="input" type="text" value={user.address} placeholder="e.g 120 Sesame Street" onChange={(evt)=>{
+                                    setUser({
+                                        ...user,
+                                         address:evt.target.value
+                                    })
+                                }}/>
                             </div>
                     </div>
                         
-                    <div class="field">
-                            <label class="label has-text-white">D.O.B</label>
-                            <div class="control">
-                                <input class="input" type="date" placeholder="e.g. alexsmith@gmail.com"/>
-                            </div>
-                    </div>
+                   
 
                         <div class="field">
                             <label class="label has-text-white">Email</label>
                             <div class="control">
-                                <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+                                <input class="input" type="email" value={user.email} placeholder="e.g.alexsmith@gmail.com" onChange={(evt)=>{
+                                    setUser({
+                                        ...user,
+                                         email:evt.target.value
+                                    })
+                                }}/>
                             </div>
                     </div>
                     <div class="field">
                             <label class="label has-text-white">Password</label>
                             <div class="control">
-                                <input class="input" type="email" placeholder="e.g. 1234"/>
+                                <input class="input" type="password" value={user.password} placeholder="e.g. 1234" onChange={(evt)=>{
+                                     setUser({
+                                         ...user,
+                                          password:evt.target.value
+                                     })
+                                }}/>
                             </div>
                     </div>
 
                     <div class="field is-grouped is-grouped-centered">
                         <p class="control">
-                            <a class="button is-primary">
-                            Submit
-                            </a>
+                            <input className="button is-primary" type="submit" name="Submit" value="Submit"/>
                         </p>
                         <p class="control">
                             <a class="button is-light">
@@ -53,6 +90,7 @@ const SignUp = () => {
                         </p>
                         </div>
             </div>
+            </form>
             
         </div>
         </div>
